014dbc592d9edcb0c789a75100056df9e07b1f24
Eduardo Talavera Ramos <eduardo.talavera245244@potros.itson.edu.mx>
2024-03-08 18:54:44 -0700

    Formato
    Agregué  formato al código.

--- flashy/script.js
+++ flashy/script.js
@@ -1,19 +1,19 @@
 // script to change the color of logo and navlinks when scrolled down
-document.addEventListener('scroll', ()=> {
+document.addEventListener('scroll', () => {
     const logo = document.getElementById('logo');
     const navBtn = document.getElementById('navBtn');
     const navbarlink = document.querySelectorAll(".navbar-link");
 
-    if(window.scrollY > 670 && window.scrollY < 2085) {
+    if (window.scrollY > 670 && window.scrollY < 2085) {
         // logo
         logo.classList.add('scrolled');
-        
+
         //navbar button
         navBtn.classList.remove('navBtn');
         navBtn.classList.add('scr');
 
         //navbar links
-        navbarlink.forEach(function(link) {
+        navbarlink.forEach(function (link) {
             link.classList.add('scrLink');
         });
 
@@ -26,10 +26,10 @@ document.addEventListener('scroll', ()=> {
         navBtn.classList.remove('scr');
 
         //navbarlinks
-        navbarlink.forEach(function(link) {
+        navbarlink.forEach(function (link) {
             link.classList.remove('scrLink');
         });
-       
+
     }
 
     console.log(window.scrollY);
